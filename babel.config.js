78b2b3faa2c59f4f352763128fda693280734522
Bruno <brunorios2009@hotmail.com>
2019-08-27 19:28:19 -0300

    unit testing aun con errores, agregue .babelrc y modifique babel.config y package.json| agregue no verify porque hay 150 errores

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   presets: [
-    '@vue/app'
-  ]
+    '@vue/app', ['env', { 'modules': false }]
+  ],
+  'env': {
+    'test': {
+      'presets': [['env', { 'targets': { 'node': 'current' } }]]
+    }
+  }
 }
